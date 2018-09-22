ee720a9a6cb94db580f96c06b89086d371c52bdf
火之玉 <1533607721@qq.com>
2018-09-22 16:06:34 +0800

    Algorithmic submit

---
+++ AlgorithmicPractice/AlgorithmicPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmicPractice
+//
+//  Created by 帅斌 on 2018/9/22.
+//  Copyright © 2018年 personal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
