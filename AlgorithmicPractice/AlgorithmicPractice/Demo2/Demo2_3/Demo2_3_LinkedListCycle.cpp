e5273ff5c6241e6e8c8ce388c15f4b8c268f2877
火之玉 <1533607721@qq.com>
2019-02-19 23:34:23 +0800

    提交修改

---
+++ AlgorithmicPractice/AlgorithmicPractice/Demo2/Demo2_3/Demo2_3_LinkedListCycle.cpp
@@ -0,0 +1,26 @@
+//
+//  Demo2_3_LinkedListCycle.cpp
+//  AlgorithmicPractice
+//
+//  Created by 帅斌 on 2019/2/19.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+#include "Demo2_3_LinkedListCycle.hpp"
+
+
+bool hasCycle(ListNode *head) {
+    ListNode* slowerNode = head;
+    ListNode* fasterNode = head;
+    
+    while(slowerNode != NULL && fasterNode != NULL && fasterNode->next != NULL){
+        slowerNode = slowerNode->next;
+        fasterNode = fasterNode->next->next;
+        
+        if(slowerNode == fasterNode){
+            return true;
+        }
+    }
+    
+    return false;
+}
---
+++ AlgorithmicPractice/AlgorithmicPractice/Demo2/Demo2_3/Demo2_3_LinkedListCycle.hpp
@@ -0,0 +1,18 @@
+//
+//  Demo2_3_LinkedListCycle.hpp
+//  AlgorithmicPractice
+//
+//  Created by 帅斌 on 2019/2/19.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+#ifndef Demo2_3_LinkedListCycle_hpp
+#define Demo2_3_LinkedListCycle_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include "Demo2_1_singleLinkedListFlip.hpp"
+
+bool hasCycle(ListNode *head);
+
+#endif /* Demo2_3_LinkedListCycle_hpp */
