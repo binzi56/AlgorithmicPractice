4947701fd3b31665f02e48adab579870202afc85
binzi56 <1533607721@qq.com>
2020-05-04 16:50:02 +0800

    1

--- AlgorithmicPractice/AlgorithmicPractice/Demo1/String/Demo1_1_4/Demo1_1_4_StringToInteger (atoi).cpp
+++ AlgorithmicPractice/AlgorithmicPractice/Demo1/String/Demo1_1_4/Demo1_1_4_StringToInteger (atoi).cpp
@@ -9,27 +9,27 @@
 #include "Demo1_1_4_StringToInteger (atoi).hpp"
 
 int myAtoi(string str) {
-    int index=0;
-    while(str[index]==' ')index++;
-    int flag=1;
+    int index = 0;
+    while(str[index]==' ') index++;
+    int flag = 1;
     if(str[index]=='-'){
         index++;
-        flag=-1;
+        flag = -1;
     }else if(str[index]=='+'){
         index++;
     }
-    int res=0;
+    int res = 0;
     while(index < str.size()){
         if(str[index] < '0' || str[index] > '9'){
             return flag * res;
         }
-        int digit=str[index]-'0';
-        if(flag==1 && res*10.0+digit>=INT_MAX){
+        int digit = str[index]-'0';
+        if(flag == 1 && res * 10.0 + digit >= INT_MAX){
             return INT_MAX;
-        }else if(flag==-1 && -(res*10.0+digit)<=INT_MIN){
+        }else if(flag == -1 && -(res * 10.0 + digit) <= INT_MIN){
             return INT_MIN;
         }
-        res = res*10+digit;
+        res = res * 10 + digit;
         index++;
     }
     return flag*res;
