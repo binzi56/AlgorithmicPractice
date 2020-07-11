47c403863a0a085751cf133de056150afecf2874
binzi56 <1533607721@qq.com>
2020-07-11 17:15:08 +0800

    1

--- AlgorithmicPractice/AlgorithmicPractice/Demo2/Demo2_3/Demo2_3_LinkedListCycle.cpp
+++ AlgorithmicPractice/AlgorithmicPractice/Demo2/Demo2_3/Demo2_3_LinkedListCycle.cpp
@@ -8,18 +8,17 @@
 
 #include "Demo2_3_LinkedListCycle.hpp"
 
+// 思路：快慢指针 快慢指针相同则有环，证明：如果有环每走一步快慢指针距离会减1
 bool hasCycle(ListNode *head) {
-    ListNode* slowerNode = head;
-    ListNode* fasterNode = head;
-    
-    while(slowerNode != NULL && fasterNode != NULL && fasterNode->next != NULL){
-        slowerNode = slowerNode->next;
-        fasterNode = fasterNode->next->next;
-        
-        if(slowerNode == fasterNode){
-            return true;
-        }
+    if(head == NULL) return false;
+
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while(fast != NULL && fast->next != NULL){
+        if(fast == slow) return true;
+        fast = fast->next->next;
+        slow = slow->next;
     }
-    
+
     return false;
 }
