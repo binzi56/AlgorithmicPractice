9ac91576e7b5b2bc5de453574afb8edfb2e08f7f
binzi56 <1533607721@qq.com>
2020-10-03 10:13:51 +0800

    1

--- AlgorithmicPractice/AlgorithmicPractice/Demo8/Demo8_1/Demo8_1_TwoSum.cpp
+++ AlgorithmicPractice/AlgorithmicPractice/Demo8/Demo8_1/Demo8_1_TwoSum.cpp
@@ -23,3 +23,21 @@ vector<int> twoSum(vector<int> nums,int target){
     }
     return res;
 }
+
+
+vector<int> twoSum1(vector<int>& nums, int target) {
+   vector<int> res;
+   if(nums.empty()) return res;
+
+   for(int i = 0; i < nums.size(); i++){
+      for(int j = i + 1; j < nums.size(); j++){
+          if(nums[i] + nums[j] == target){
+              res.push_back(i);
+              res.push_back(j);
+              break;
+          }
+      }
+   }
+
+   return res;
+}
